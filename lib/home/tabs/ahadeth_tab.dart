2273c144a0d078e01b819d4c6082704f593a0cd7
Ahmed <ahmedm.81992@gmail.com>
2024-02-19 19:24:13 +0200

    Making of ahadeth_tab.dart model.

--- lib/home/tabs/ahadeth_tab.dart
+++ lib/home/tabs/ahadeth_tab.dart
@@ -1,11 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
-class AhadethTab extends StatelessWidget {
+import '../../models/hadeth_model.dart';
+
+class AhadethTab extends StatefulWidget {
   //static const String routeName = "MoshafPage";
-  const AhadethTab({super.key});
+  AhadethTab({super.key});
+
+  @override
+  State<AhadethTab> createState() => _AhadethTabState();
+}
+
+class _AhadethTabState extends State<AhadethTab> {
+  List<HadethModel> ahadethData = [];
 
   @override
   Widget build(BuildContext context) {
+    if (ahadethData.isEmpty) {
+      loadHadethFile();
+    }
     return Column(
       children: [
         Image.asset("assets/images/ahadeth_tab/ahadeth_logo.png"),
@@ -21,9 +34,38 @@ class AhadethTab extends StatelessWidget {
           thickness: 3,
           color: Color(0xFFB7935F),
         ),
+        Expanded(
+          child: ListView.builder(
+            itemBuilder: (context, index) {
+              return Text(
+                ahadethData[index].title,
+                textAlign: TextAlign.center,
+              );
+            },
+            itemCount: ahadethData.length,
+          ),
+        )
       ],
     );
   }
-}
 
-loadHadethFile() {}
+  loadHadethFile() {
+    /*String hadethFile =
+      await rootBundle.loadString("assets/files/ahadeth/ahadeth.txt");*/
+    rootBundle
+        .loadString("assets/files/ahadeth/ahadeth.txt")
+        .then((hadethFile) {
+      List<String> ahadeth = hadethFile.split("#");
+
+      for (int i = 0; i < ahadeth.length; i++) {
+        String hadeth = ahadeth[i];
+        List<String> hadethLines = hadeth.split("\n");
+        String title = hadethLines[0];
+        hadethLines.removeAt(0);
+        List<String> hadethContent = hadethLines;
+        ahadethData.add(HadethModel(title: title, content: hadethContent));
+      }
+      setState(() {});
+    }); //we use this if we dont want to use await.
+  }
+}
